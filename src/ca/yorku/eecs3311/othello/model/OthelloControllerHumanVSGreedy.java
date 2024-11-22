3f75699fe1780b703a6c50377ad9378ed5922c76
NamDo8467 <huynhnamdo@gmail.com>
2024-11-22 09:49:35 -0500

    added stage parameter

--- src/ca/yorku/eecs3311/othello/model/OthelloControllerHumanVSGreedy.java
+++ src/ca/yorku/eecs3311/othello/model/OthelloControllerHumanVSGreedy.java
@@ -1,4 +1,7 @@
 package ca.yorku.eecs3311.othello.model;
+
+import javafx.stage.Stage;
+
 /**
  * This controller uses the Model classes to allow the Human player P1 to play
  * the computer P2. The computer, P2 uses a greedy strategy. 
@@ -8,8 +11,8 @@ package ca.yorku.eecs3311.othello.model;
  */
 public class OthelloControllerHumanVSGreedy extends OthelloControllerVerbose {
 
-	public OthelloControllerHumanVSGreedy() {
-		super();
+	public OthelloControllerHumanVSGreedy(Stage stage) {
+		super(stage);
 		this.player1 = new PlayerHuman(this.othello, OthelloBoard.P1);
 		this.player2 = new PlayerGreedy(this.othello, OthelloBoard.P2);
 	}
@@ -22,7 +25,7 @@ public class OthelloControllerHumanVSGreedy extends OthelloControllerVerbose {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		OthelloControllerHumanVSGreedy oc = new OthelloControllerHumanVSGreedy();
-		oc.play();
+//		OthelloControllerHumanVSGreedy oc = new OthelloControllerHumanVSGreedy();
+//		oc.play();
 	}
 }
