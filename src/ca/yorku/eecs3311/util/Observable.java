636b198e5edd7bb36f9d06f83f2959b6eb543d52
NamDo8467 <huynhnamdo@gmail.com>
2024-11-23 00:20:34 -0500

    remove spaces and comments

--- src/ca/yorku/eecs3311/util/Observable.java
+++ src/ca/yorku/eecs3311/util/Observable.java
@@ -12,10 +12,12 @@ public class Observable {
 	private ArrayList<Observer> observers = new ArrayList<Observer>();
 	public void attach(Observer o) {
 		observers.add(o);
+		
 	}
 	public void detach(Observer o) {
 		observers.remove(o);
 	}
+	
 	public void notifyObservers() {
 		for(Observer o:observers) {
 			o.update(this);
