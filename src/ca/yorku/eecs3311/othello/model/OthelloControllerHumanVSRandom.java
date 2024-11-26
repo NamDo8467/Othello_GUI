e1914bbc0c703678c42b8a6d5270195daf47daeb
NamDo8467 <huynhnamdo@gmail.com>
2024-11-26 14:35:36 -0500

    remove extends keywork

--- src/ca/yorku/eecs3311/othello/model/OthelloControllerHumanVSRandom.java
+++ src/ca/yorku/eecs3311/othello/model/OthelloControllerHumanVSRandom.java
@@ -1,7 +1,10 @@
 package ca.yorku.eecs3311.othello.model;
 
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
  * This controller uses the Model classes to allow the Human player P1 to play
@@ -22,10 +25,63 @@ public class OthelloControllerHumanVSRandom extends OthelloControllerVerbose imp
 		this.player2 = new PlayerRandom(this.othello, OthelloBoard.P2);
 	}
 	
-	@Override
 	public void handleMove(int row, int col, GridPane gridBoard, Othello othello) {
-		// TODO Auto-generated method stub
-		
+		if (othello.isGameOver() == false) {
+			char whosTurn = othello.getWhosTurn();
+			Move move = null;
+			move = new Move(row, col);
+			
+			boolean canMove = othello.move(move.getRow(), move.getCol());
+			if (canMove) {
+				char [][] boardArray = othello.getBoardArray();
+				
+				Color color = Color.RED;
+				if (whosTurn == OthelloBoard.P1) {
+					color = Color.BLACK;
+				}
+				if (whosTurn == OthelloBoard.P2) {
+					color = Color.WHITE;
+				}
+
+				Circle piece = new Circle(70 / 2 - 5);
+		        piece.setFill(color);
+		        gridBoard.add(piece, col, row);
+		        
+		        // Center the piece in the cell
+		        GridPane.setHalignment(piece, HPos.CENTER);
+		        GridPane.setValignment(piece, VPos.CENTER);
+		        
+		        // Update the GUI based on the board string array
+		        for (int r = 0; r < boardArray.length; r++) {
+					for(int c = 0; c < boardArray[0].length; c++) {
+						Color updatedColor = Color.RED;
+						if (boardArray[r][c] == OthelloBoard.P1) {
+							updatedColor = Color.BLACK;
+							Circle updatedPiece = new Circle(70 / 2 - 5);
+					        updatedPiece.setFill(updatedColor);
+					        gridBoard.add(updatedPiece, c, r);
+
+					     // Center the piece in the cell
+					        GridPane.setHalignment(updatedPiece, HPos.CENTER);
+					        GridPane.setValignment(updatedPiece, VPos.CENTER);
+							
+						}else if(boardArray[r][c] == OthelloBoard.P2){
+							updatedColor = Color.WHITE;
+							Circle updatedPiece = new Circle(70 / 2 - 5);
+					        updatedPiece.setFill(updatedColor);
+					        gridBoard.add(updatedPiece, c, r);
+					     // Center the piece in the cell
+					        GridPane.setHalignment(updatedPiece, HPos.CENTER);
+					        GridPane.setValignment(updatedPiece, VPos.CENTER);
+						}
+
+					}
+				}
+			}
+			
+		}else {
+			othello.notifyObservers();
+		}
 	}
 	/**
 	 * Run main to play a Human (P1) against the computer P2. 
