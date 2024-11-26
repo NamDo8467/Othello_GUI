0c36663e40234e53d7b283525c3501f882052ab4
NamDo8467 <huynhnamdo@gmail.com>
2024-11-26 14:30:20 -0500

    added print statement

--- src/ca/yorku/eecs3311/util/Observable.java
+++ src/ca/yorku/eecs3311/util/Observable.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class Observable {
 	private ArrayList<Observer> observers = new ArrayList<Observer>();
 	public void attach(Observer o) {
-		observers.add(o);
+		this.observers.add(o);
 		
 	}
 	public void detach(Observer o) {
