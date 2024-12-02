54108988d0f08622dcda3648679b0ed2142bd039
NamDo8467 <huynhnamdo@gmail.com>
2024-12-02 14:42:45 -0500

    seperate the update GUI functionality and capture the current game state

--- src/ca/yorku/eecs3311/othello/model/OthelloControllerHumanVSHuman.java
+++ src/ca/yorku/eecs3311/othello/model/OthelloControllerHumanVSHuman.java
@@ -1,7 +1,6 @@
 package ca.yorku.eecs3311.othello.model;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
-
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -18,24 +17,32 @@ public class OthelloControllerHumanVSHuman extends OthelloControllerVerbose impl
 	 * Constructs a new OthelloController with a new Othello game, ready to play
 	 * with two users at the console.
 	 */
-	private PlayerHuman player1;
-	private PlayerHuman player2;
 	public OthelloControllerHumanVSHuman(Othello othello) {
 		this.player1 = new PlayerHuman(othello, OthelloBoard.P1);
 		this.player2 = new PlayerHuman(othello, OthelloBoard.P2);
+		ScenePlayerVSAny scenePlayerVSPlayer = new ScenePlayerVSPlayer(othello);
+		othello.stage.setScene(scenePlayerVSPlayer.getScene());
 	}
 	
 	public void handleMove(int row, int col, GridPane gridBoard, Othello othello) {
 		if (othello.isGameOver() == false) {
 			char whosTurn = othello.getWhosTurn();
+			
+			OthelloBoard boardArrayCopy = othello.getBoard();
+			char [][] b = boardArrayCopy.board;
 			Move move = null;
 			move = new Move(row, col);
 			
+			Othello othelloCopy = othello.copy();
+			
+			GameState gameState = new GameState(othelloCopy.getBoard(), othello.getWhosTurn(), othello.getBoard().copyTokens());
+//			System.out.println("-----------------------");
 			boolean canMove = othello.move(move.getRow(), move.getCol());
+			
+			
 			if (canMove) {
-				char [][] boardArray = othello.getBoardArray();
-				
 				Color color = Color.RED;
+				char [][] boardArray = othello.getBoardArray();
 				if (whosTurn == OthelloBoard.P1) {
 					color = Color.BLACK;
 				}
@@ -51,42 +58,50 @@ public class OthelloControllerHumanVSHuman extends OthelloControllerVerbose impl
 		        // Center the piece in the cell
 		        GridPane.setHalignment(token, HPos.CENTER);
 		        GridPane.setValignment(token, VPos.CENTER);
-		        
-		        // Update the GUI based on the board string array
-		        for (int r = 0; r < boardArray.length; r++) {
-					for(int c = 0; c < boardArray[0].length; c++) {
-						Color updatedColor = Color.RED;
-						if (boardArray[r][c] == OthelloBoard.P1) {
-							updatedColor = Color.BLACK;
-							Circle updatedToken = new Circle(70 / 2 - 5);
-							
-							updatedToken.setFill(updatedColor);
-					        gridBoard.add(updatedToken, c, r);
-					        othello.addTokenToTokenList(updatedToken);
-					        
-					     // Center the piece in the cell
-					        GridPane.setHalignment(updatedToken, HPos.CENTER);
-					        GridPane.setValignment(updatedToken, VPos.CENTER);
-					        
-							
-						}else if(boardArray[r][c] == OthelloBoard.P2){
-							updatedColor = Color.WHITE;
-							Circle updatedToken = new Circle(70 / 2 - 5);
-							updatedToken.setFill(updatedColor);
-					        gridBoard.add(updatedToken, c, r);
-					        othello.addTokenToTokenList(updatedToken);
-					        
-					     // Center the piece in the cell
-					        GridPane.setHalignment(updatedToken, HPos.CENTER);
-					        GridPane.setValignment(updatedToken, VPos.CENTER);
-						}
 
-					}
-				}
+		        
+		        // Update the history stack
+//		        System.out.println(othello.getCount(OthelloBoard.P1));
+//				System.out.println(whosTurn);
+		        
+		        
+//				System.out.println(gameState.getTokens().size());
+		        othello.historyStack.push(gameState);
+		        othello.notifyObservers();
+		        
+		        // Update the GUI based on the board string array		    
+//		        for (int r = 0; r < boardArray.length; r++) {
+//					for(int c = 0; c < boardArray[0].length; c++) {
+//						Color updatedColor = Color.RED;
+//						if (boardArray[r][c] == OthelloBoard.P1) {
+//							updatedColor = Color.BLACK;
+//							Circle updatedToken = new Circle(70 / 2 - 5);
+//							
+//							updatedToken.setFill(updatedColor);
+//					        gridBoard.add(updatedToken, c, r);
+//					        othello.addTokenToTokenList(updatedToken);
+//					        
+//					     // Center the piece in the cell
+//					        GridPane.setHalignment(updatedToken, HPos.CENTER);
+//					        GridPane.setValignment(updatedToken, VPos.CENTER);
+//					        
+//							
+//						}else if(boardArray[r][c] == OthelloBoard.P2){
+//							updatedColor = Color.WHITE;
+//							Circle updatedToken = new Circle(70 / 2 - 5);
+//							updatedToken.setFill(updatedColor);
+//					        gridBoard.add(updatedToken, c, r);
+//					        othello.addTokenToTokenList(updatedToken);
+//					        
+//					     // Center the piece in the cell
+//					        GridPane.setHalignment(updatedToken, HPos.CENTER);
+//					        GridPane.setValignment(updatedToken, VPos.CENTER);
+//						}
+//
+//					}
+//				}
 			}
 			
-		}else {
-			othello.notifyObservers();
 		}
 	}
 
