e0d94594e79a7e8b829579c8a783a8812448fa6f
NamDo8467 <huynhnamdo@gmail.com>
2024-12-06 23:34:57 -0500

    added save game feature

--- src/ca/yorku/eecs3311/othello/model/ScenePlayerVSAny.java
+++ src/ca/yorku/eecs3311/othello/model/ScenePlayerVSAny.java
@@ -1,20 +1,19 @@
 package ca.yorku.eecs3311.othello.model;
-
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-
 import ca.yorku.eecs3311.othello.viewcontroller.OthelloApplication;
 import ca.yorku.eecs3311.othello.viewcontroller.VScoreAndTurn;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
@@ -30,6 +29,8 @@ public abstract class ScenePlayerVSAny {
 	private ArrayList<Rectangle> cells;
 	private ArrayList<Circle> tokens;
 	
+	private final int BUTTON_HEIGHT = 30;
+	private final int BUTTON_WIDTH = 55;
 
 	
 	public ScenePlayerVSAny(Othello othello) {
@@ -48,91 +49,78 @@ public abstract class ScenePlayerVSAny {
 		Button restartButton = new Button("Restart");
 		
 		Button undoButton = new Button("Undo");
+		
+		Button saveGameButton = new Button("Save game");
 
 		restartButton.setFocusTraversable(false);
-		restartButton.setPrefWidth(55);
-		restartButton.setPrefHeight(30);
+		restartButton.setPrefWidth(BUTTON_WIDTH);
+		restartButton.setPrefHeight(BUTTON_HEIGHT);
 		
 		restartButton.setOnMouseClicked((MouseEvent event) ->{
 			this.othello.resetBoard();
 		});
 		
 		undoButton.setFocusTraversable(false);
-		undoButton.setPrefWidth(55);
-		undoButton.setPrefHeight(30);
+		undoButton.setPrefWidth(BUTTON_WIDTH);
+		undoButton.setPrefHeight(BUTTON_HEIGHT);
 		
 		undoButton.setOnMouseClicked((MouseEvent event) -> {
 			this.pressButton();
 		});
 		
+		saveGameButton.setFocusTraversable(false);
+		saveGameButton.setPrefWidth(BUTTON_WIDTH + 20);
+		saveGameButton.setPrefHeight(BUTTON_HEIGHT);
+		
+		saveGameButton.setOnMouseClicked((MouseEvent event) ->{
+			Othello othelloCopy = this.othello.copy();
+			
+			// Capture the current state of the game
+			GameState gameState = new GameState(othelloCopy.getBoard(), othello.getWhosTurn(), othello.getBoard().copyTokens());
+		
+			try {
+		      FileOutputStream file = new FileOutputStream("saved_game.txt");
+		      
+		      OutputStreamWriter outputStreamWriter = new OutputStreamWriter(file);
+		      char [][] currentBoard = gameState.getBoard().board;
+		   	    
+		      outputStreamWriter.write("mode:" + othelloCopy.mode);
+		      outputStreamWriter.write("\n");
+		      outputStreamWriter.write("whosTurn:" + othelloCopy.getWhosTurn());
+		      outputStreamWriter.write("\n");
+		      int rows = currentBoard.length;
+		      int cols = currentBoard[0].length;
+		      
+		      for (int i = 0; i < rows; i++) {
+		    	  for (int j = 0; j < cols; j++) {
+					outputStreamWriter.write(currentBoard[i][j]);
+				}
+		    	  outputStreamWriter.write("\n");
+				
+		      }
+		      
+		      
+		      outputStreamWriter.close();
+		      
+		    } catch (FileNotFoundException e) {
+		      System.out.println("Saved game file not found");
+//		      e.printStackTrace();
+		    }catch (IOException e2) {
+		    	System.out.println("An error occurred while attempting to save the file");
+			}
+		
+		});
+		
 		bp.setRight(buttonBox);
 		bp.setBottom(score);
 		
-		buttonBox.getChildren().addAll(restartButton, undoButton);
+		buttonBox.getChildren().addAll(restartButton, undoButton, saveGameButton);
 		buttonBox.setAlignment(Pos.CENTER);
 		
 		bp.setCenter(this.othello.getBoard().gridBoard);
 		this.othello.attach(score);
 
 		this.scene = new Scene(bp, OthelloApplication.SCENE_WIDTH, OthelloApplication.SCENE_HEIGHT);
-
-//		this.othello.stage.setScene(scene);
-		
-		
-		
-		
-		
-//		for (int row = 0; row < Othello.DIMENSION; row++) {
-//            for (int col = 0; col < Othello.DIMENSION; col++) {
-//                Rectangle tile = new Rectangle(OthelloBoard.CELL_WIDTH, OthelloBoard.CELL_HEIGHT);
-//                
-//                // Set color for a checkerboard pattern
-//                tile.setFill((row + col) % 2 == 0 ? Color.GREEN : Color.DARKGREEN);
-//                
-//                gridBoard.add(tile, col, row);
-//                
-//                
-//        		if ((row == 3 && col == 3) || (row == 4 && col == 4)) {
-////        			System.out.println("Row: " + row + " Col: " + col);
-//                	Circle token = new Circle(OthelloBoard.CELL_SIZE / 2 - 5);
-//                    
-//                    token.setFill(Color.BLACK);
-//                    gridBoard.add(token, col, row);
-//                    tokens.add(token);
-//                    GridPane.setHalignment(token, HPos.CENTER);
-//                    GridPane.setValignment(token, VPos.CENTER);
-//                               
-//        		}
-//
-//                if((row == 4 && col == 3) || (row == 3 && col == 4)) {
-////                	System.out.println("Row: " + row + " Col: " + col);
-//                	Circle token = new Circle(OthelloBoard.CELL_SIZE / 2 - 5);
-//                    
-//                    token.setFill(Color.WHITE);
-//                    gridBoard.add(token, col, row);
-//                    tokens.add(token);
-////                    System.out.println(tokens);
-//                    GridPane.setHalignment(token, HPos.CENTER);
-//                    GridPane.setValignment(token, VPos.CENTER);
-//                    
-//                    
-//                }
-//                
-//                
-//                
-//                cells.add(tile);
-//            }
-//
-//	
-//
-//		}
-//		
-//		 
-//		gridBoard.setAlignment(Pos.CENTER);
-//		
-//		gridBoard.setGridLinesVisible(true);
-		
-		
 	}
 	
 	public Scene getScene() {
@@ -147,9 +135,9 @@ public abstract class ScenePlayerVSAny {
         command.execute();
     }
 	
-	public void addTheFirst4Token(int row, int col) {
+	public void saveGame() {
 		
-
+		
 	}
 
 	
